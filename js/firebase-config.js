8da98ab72becb0a49f2dc3532d6205e122c7d454
Marney Samuell <marneysamuellaraujo2@gmail.com>
2026-07-13 21:54:37 -0300

    up

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,12 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDHWdwZ-SyOZjvUv9blUv1m70m5CvaOs8o",
   authDomain: "erp-ateliedebora-275e7.firebaseapp.com",
   databaseURL: "https://erp-ateliedebora-275e7-default-rtdb.firebaseio.com",
   projectId: "erp-ateliedebora-275e7",
+  storageBucket: "erp-ateliedebora-275e7.firebasestorage.app",
 };
 
 // Start Firebase App
@@ -17,6 +19,7 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const database = getDatabase(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 
 // Expor globalmente para manter compatibilidade temporária com scripts inline existentes do legado
 window.auth = auth;
